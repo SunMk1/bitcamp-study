844003b12b36e1886bda82a8aac61270c1af6a00
kmk3296 <kmk3296@gmail.com>
2021-12-15 18:09:15 +0900

    ,

--- java-lang-boot/app/src/main/java/com/eomcs/study/App.java
+++ java-lang-boot/app/src/main/java/com/eomcs/study/App.java
@@ -1,13 +1,11 @@
 package com.eomcs.study;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
+//@SpringBootApplication
+//@RestController
 public class App {
 
   public static void main(String[] args) {
@@ -16,6 +14,6 @@ public class App {
 
   @GetMapping("/hello")
   public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-    return String.format("Hello %s!", name);
+    return "Hello, java-lang-boot!";
   }
 }
