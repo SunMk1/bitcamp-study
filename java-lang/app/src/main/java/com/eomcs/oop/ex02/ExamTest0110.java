e88e1b60aaf9c798b8a67859adf3e009f3e85901
kmk3296 <kmk3296@gmail.com>
2022-01-04 20:54:51 +0900

    .

---
+++ java-lang/app/src/main/java/com/eomcs/oop/ex02/ExamTest0110.java
@@ -0,0 +1,7 @@
+package com.eomcs.oop.ex02;
+
+public class ExamTest0110 {
+
+  public static void main(String[] args) {
+  }
+}
---
+++ java-lang/app/src/main/java/com/eomcs/oop/ex02/ExamTest0210.java
@@ -0,0 +1,7 @@
+package com.eomcs.oop.ex02;
+
+public class ExamTest0210 {
+
+  public static void main(String[] args) {
+  }
+}
