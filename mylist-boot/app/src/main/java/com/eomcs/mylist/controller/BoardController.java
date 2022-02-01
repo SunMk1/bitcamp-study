0abe25b83230e740780a97d2cf769b9e040f6b3e
kmk3296 <kmk3296@gmail.com>
2022-02-01 19:16:33 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BoardController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BoardController.java
@@ -1,88 +1,58 @@
 package com.eomcs.mylist.controller;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
 import java.sql.Date;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.eomcs.mylist.dao.CsvBoardDao;
 import com.eomcs.mylist.domain.Board;
-import com.eomcs.util.ArrayList;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class BoardController {
 
-  ArrayList boardList = new ArrayList();
-
-  public BoardController() throws Exception {
-    System.out.println("BoardController() 호출됨!");
-
-    try {
-      BufferedReader in = new BufferedReader(new FileReader("boards.json"));
-      boardList = new ArrayList(new ObjectMapper().readValue(in.readLine(), Board[].class));
-
-      in.close();
-    } catch (Exception e) {
-      System.out.println("데이터 로딩중에 오류발생");
-    }
-  }
+  CsvBoardDao boardDao = new CsvBoardDao();
 
   @RequestMapping("/board/list")
   public Object list() { 
-    return boardList.toArray();
+    return boardDao.findAll();
   }
 
   @RequestMapping("/board/add")
   public Object add(Board board) {
     board.setCreatedDate(new Date(System.currentTimeMillis()));
-    boardList.add(board);
+    boardDao.insert(board);
 
-    return boardList.size();
+    return boardDao.countAll();
   }
 
   @RequestMapping("/board/get")
   public Object get(int index) {
-    if (index < 0 || index >= boardList.size()) { 
+    Board board = boardDao.findByNo(index);
+    if (board == null) { 
       return "";
     }
-    Board board = (Board) boardList.get(index);
     board.setViewCount(board.getViewCount() + 1);
     return board;
   }
 
   @RequestMapping("/board/update")
   public Object update(int index, Board board) {
-    if (index < 0 || index >= boardList.size()) { 
+    Board old = boardDao.findByNo(index);
+    if (old == null) { 
       return 0;
     }
-    Board old = (Board) boardList.get(index);
     board.setViewCount(old.getViewCount());
     board.setCreatedDate(old.getCreatedDate());
-    return boardList.set(index, board) == null ? 0 : 1;
+    return boardDao.update(index, board);
   }
 
   @RequestMapping("/board/delete")
   public Object delete(int index) {
-    if (index < 0 || index >= boardList.size()) {
-      return 0;
-    }
-    return boardList.remove(index) == null ? 0 : 1;
+    return boardDao.delet(index);
   }
 
   @RequestMapping("/board/save")
   public Object save() throws Exception {
-    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("boards.json")));
-
-    ObjectMapper mapper = new ObjectMapper();
-
-    String jsonStr = mapper.writeValueAsString(boardList.toArray());
-
-    out.println(jsonStr);
-
-    out.close();
-    return boardList.size();
+    boardDao.save();
+    return boardDao.countAll();
   }
 }
