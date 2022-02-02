cb9449a3bf289d9e25aeb0fd280268aa0ab6aace
kmk3296 <kmk3296@gmail.com>
2022-02-02 18:55:05 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/BinaryBoardDao.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/BinaryBoardDao.java
@@ -1,25 +1,34 @@
 package com.eomcs.mylist.dao;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.sql.Date;
 import com.eomcs.mylist.domain.Board;
 import com.eomcs.util.ArrayList;
 
-public class CsvBoardDao {
+public class BinaryBoardDao {
   ArrayList boardList = new ArrayList();
 
-  String filename = "boards.csv";
+  String filename = "boards.bin";
 
-  public CsvBoardDao() {
+  public BinaryBoardDao() {
     try {
-      BufferedReader in = new BufferedReader(new FileReader(filename));
+      DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(filename)));
 
-      String csvStr;
-      while ((csvStr = in.readLine()) != null) {
-        boardList.add(Board.valueOf(csvStr)); 
+      int len = in.readInt();
+
+      for (int i = 0; i < len; i++) {
+        Board board = new Board();
+        board.setTitle(in.readUTF());
+        board.setContent(in.readUTF());
+        board.setViewCount(in.readInt());
+        board.setCreatedDate(Date.valueOf(in.readUTF()));
+
+        boardList.add(board);
       }
       in.close();
     } catch (Exception e) {
@@ -28,12 +37,16 @@ public class CsvBoardDao {
   }
 
   public void save() throws Exception{
-    PrintWriter out = new PrintWriter (new BufferedWriter(new FileWriter(filename)));
+    DataOutputStream out = new DataOutputStream (new BufferedOutputStream(new FileOutputStream(filename)));
 
-    Object[] arr = boardList.toArray();
+    out.writeInt(boardList.size());
+
     for (int i = 0; i < boardList.size(); i++) {
       Board board = (Board) boardList.get(i);
-      out.println(board.toCsvString());
+      out.writeUTF(board.getTitle());
+      out.writeUTF(board.getContent());
+      out.writeInt(board.getViewCount());
+      out.writeUTF(board.getCreatedDate().toString());
     }
     out.flush();
 
