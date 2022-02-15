6668df7ea3426444246abc05f71df8aa6803e0b6
kmk3296 <kmk3296@gmail.com>
2022-02-15 18:02:17 +0900

    .

---
+++ java-lang/app/src/main/java/com/eomcs/algorithm/data_structure/array2/step3/Test2.java
@@ -0,0 +1,21 @@
+package com.eomcs.algorithm.data_structure.array2.step3;
+
+import com.eomcs.algorithm.data_structure.array2.Score;
+
+public class Test2 {
+  public static void main(String[] args) {
+    ArrayList<Score> list = new ArrayList<Score>();
+    list.add(new Score("홍길동", 100, 100 ,100));
+    list.add(new Score("임꺽정", 100, 100 ,100));
+    list.add(new Score("유관순", 100, 100 ,100));
+    list.add(new Score("안중근", 100, 100 ,100));
+    list.add(new Score("윤봉길", 100, 100 ,100));
+
+    //    list.add(new String("오호라"));
+
+    for (int i = 0; i < list.size(); i++) {
+      Score s = list.get(i);
+      System.out.printf("%s: %d, %f\n", s.name, s.sum, s.aver);
+    }
+  }
+}
