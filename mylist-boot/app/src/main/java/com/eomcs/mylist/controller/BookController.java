57da02518b5ba6ae4720aac0e0ac2362c0f90695
unknown <kmk3296@gmail.com>
2022-04-25 03:56:35 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BookController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BookController.java
@@ -1,8 +1,11 @@
 package com.eomcs.mylist.controller;
 
+import java.io.File;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import com.eomcs.mylist.dao.BookDao;
 import com.eomcs.mylist.domain.Book;
 
@@ -18,10 +21,14 @@ public class BookController {
   }
 
   @RequestMapping("/book/add")
-  public Object add(Book book) throws Exception {
-    bookDao.insert(book);
-
-    return bookDao.countAll();
+  public Object add(Book book, MultipartFile file) throws Exception {
+    try {
+      book.setPhoto(saveFile(file));
+      return bookDao.insert(book);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return "error!";
+    }
   }
 
   @RequestMapping("/book/get")
@@ -31,12 +38,34 @@ public class BookController {
   }
 
   @RequestMapping("/book/update")
-  public Object update(Book book) throws Exception {
-    return bookDao.update(book);
+  public Object update(Book book, MultipartFile file) throws Exception {
+    try {
+      book.setPhoto(saveFile(file));
+      return bookDao.update(book);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return "error!";
+    }
   }
 
   @RequestMapping("/book/delete")
   public Object delete(int no) throws Exception {
     return bookDao.delete(no);
+  }
+
+  private String saveFile(MultipartFile file) throws Exception {
+    if (file != null && file.getSize() == 0) {
+      String filename = UUID.randomUUID().toString();
+      int dotIndex = file.getOriginalFilename().lastIndexOf(".");
+      if (dotIndex != -1) {
+        filename += file.getOriginalFilename().substring(dotIndex);
+      }
+      File photoFile = new File("c:/upload/book/" + filename);
+      file.transferTo(photoFile);
+
+      return filename;
+    } else {
+      return null;
+    }
   }
 }
